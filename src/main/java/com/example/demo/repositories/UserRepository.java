f8f88eaedfe81160fb4f3201ba31aefacde3bb29
potatowagon <e0007652@u.nus.edu>
2019-12-13 01:38:03 +0800

    git init

---
+++ src/main/java/com/example/demo/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.User;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+    List<User> findByName(String name);
+    
+}
