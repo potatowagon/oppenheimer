6c81d6407b87f2ddeeba06978eb12482a25758ef
wongsh <wongsh@autodesk.com>
2019-12-13 23:30:07 +0800

    form upload

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,17 +1,22 @@
 package com.example.demo.controllers;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entities.User;
 import com.example.demo.repositories.UserRepository;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Controller
 public class UserController {
@@ -64,5 +69,20 @@ public class UserController {
         userRepository.delete(user);
         model.addAttribute("users", userRepository.findAll());
         return "index";
+    }
+
+    @PostMapping("/uploadFile")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void handleFileUpload(@RequestParam("file") MultipartFile file) throws Exception {
+        File convFile = new File( file.getOriginalFilename() );
+        FileOutputStream fos = new FileOutputStream( convFile );
+        fos.write( file.getBytes() );
+        fos.close();
+        FileReader fr = new FileReader(convFile);
+        BufferedReader br=new BufferedReader(fr);
+        String line;
+        while((line=br.readLine())!=null) {
+            System.out.println(br.readLine());
+        }
     }
 }
