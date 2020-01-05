ac93440c822c00a0a548e9b226b7a3621e9dcd99
potatowagon <e0007652@u.nus.edu>
2020-01-05 17:02:45 +0800

    unit test for BookkeeperEmployeeView mask id

---
+++ src/test/java/com/example/demo/entities/BookkeeperEmployeeViewTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.entities;
+
+import junitparams.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.*;
+import static junitparams.JUnitParamsRunner.*;
+
+@RunWith(JUnitParamsRunner.class)
+public class BookkeeperEmployeeViewTest {
+
+    @Test
+    @junitparams.Parameters({
+            "123, 123",
+            "1234, 1234",
+            "12345, 1234$",
+            "123456789, 1234$$$$$"
+    })
+    public void getMaskedId(Long id, String expectedMaskedId) {
+        Employee employee = new Employee();
+        employee.setId(id);
+        employee.setName("mock");
+        employee.setBirthday("2020-01-02");
+        employee.setSalary(0);
+        employee.setTaxPaid(0);
+        employee.setGender("");
+        assertThat(new BookkeeperEmployeeView(employee).getMaskedId()).isEqualTo(expectedMaskedId);
+    }
+
+    @Test
+    public void getTaxRelief() {
+    }
+}
